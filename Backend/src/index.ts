52d52072732ea2390bc0489d6930f7b120b8d8b2
H.M. <jame.jame9424@gmail.com>
2026-04-07 09:11:34 +0700

    start login feature

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import express, { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 
+import bcrypt from 'bcrypt';
+
 const prisma = new PrismaClient();
 const app = express();
 app.use(cors()); // 2. บรรทัดนี้ต้องอยู่ "ก่อน" app.get หรือ app.post ทุกอันครับ
@@ -61,4 +63,45 @@ app.get('/users/:userId/tasks', async (req: Request, res: Response) => {
 
 app.listen(port, () => {
     console.log(`🚀 Server is running at http://localhost:${port}`);
+});
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+
+    // 1. สร้าง "เกลือ" (Salt) เพื่อสุ่มความยากในการถอดรหัส
+    const saltRounds = 10; 
+    // 2. Hash รหัสผ่าน
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    // 3. บันทึก hashedPassword ลง Database (แทน password ตัวจริง)
+    const user = await prisma.users.create({
+        data: {
+            username: username,
+            password_hash: hashedPassword // เก็บตัวที่เขวี้ยงเข้าเครื่องปั่นแล้ว
+        }
+    });
+    res.json({ message: "สมัครสมาชิกสำเร็จ!" });
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    // 1. หา User ใน DB จากชื่อ
+    const user = await prisma.users.findUnique({
+        where: { username: username }
+    });
+
+    if (!user) {
+        return res.status(401).json({ error: "ไม่พบผู้ใช้งานนี้" });
+    }
+
+    // 2. เทียบรหัสผ่าน (Bcrypt จะเอา password ตัวจริงไปปั่นแล้วเทียบกับใน DB ให้เอง)
+    const isMatch = await bcrypt.compare(password, user.password_hash);
+
+    if (isMatch) {
+        // Login สำเร็จ! (เดี๋ยวเราจะมาทำเรื่องการส่ง Token ต่อ)
+        res.json({ message: "Login สำเร็จ!", userId: user.id });
+    } else {
+        res.status(401).json({ error: "รหัสผ่านไม่ถูกต้อง" });
+    }
 });
